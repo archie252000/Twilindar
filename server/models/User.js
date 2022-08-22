6d3af8c87dcad142d83ed577a4c079ebd380443f
Aaryavrat Arora <arc252000@gmail.com>
2022-08-23 00:38:41 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -18,7 +18,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    accessToken: {
+    accessTokenSecret: {
         type: String,
         required: true,
     },
