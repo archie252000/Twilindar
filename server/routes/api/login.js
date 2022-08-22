c748a310e271c5d69289ce418c4a11f01fb48a76
Aaryavrat Arora <arc252000@gmail.com>
2022-08-23 01:47:08 +0530

    Update login.js, Add schedule.js

--- server/routes/api/login.js
+++ server/routes/api/login.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const Router = express.Router();
+const User = require("../../models/User");
+Router.post("/", [], async(req, res) => {
 
-Router.post("/", [], async(req, res) => {});
+    const {
+        twitterUserId,
+        username,
+        name,
+        accessToken,
+        accessTokenSecret
+    } = req.body;
+
+    let user = await User.findOne()
+
+
+});
